50db8ef8b3bf3daa9b81de6a1012bef77574ebd7
Guilherme junior <mrguilher621@gmail.com>
2017-10-01 15:20:04 -0300

    Botão facebook funcionando

--- app/src/main/java/com/guilherme/projetopetcare2/Activitys/Activitys/MainActivity.java
+++ app/src/main/java/com/guilherme/projetopetcare2/Activitys/Activitys/MainActivity.java
@@ -1,23 +1,82 @@
 package com.guilherme.projetopetcare2.Activitys.Activitys;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.guilherme.projetopetcare2.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginButton = (LoginButton) findViewById(R.id.login_buttonFacebook);
+
+        callbackManager = CallbackManager.Factory.create();
         btnLogin = (Button) findViewById(R.id.btnFazerLogin);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+        catch (NoSuchAlgorithmException e) {
+
+        }
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("ID_FB",loginResult.getAccessToken().getUserId());
+                redirect(loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken != null){
+            redirect(accessToken.getUserId());
+        }
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +86,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(AbrirLogin);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void redirect(String ID){
+        Intent intent = new Intent(MainActivity.this,PrincipalActivity.class);
+        intent.putExtra("FB_ID",ID);
+        startActivity(intent);
     }
 }
